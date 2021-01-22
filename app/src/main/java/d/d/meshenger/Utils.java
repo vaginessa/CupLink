0b17bc326c06c6c9d5d1fe7f8d36f498115826bf
vadym <vadym.vikulin@gmail.com>
2021-01-22 16:39:30 +0200

    1. optimized Crypto 2. wlan delection fix

--- app/src/main/java/d/d/meshenger/Utils.java
+++ app/src/main/java/d/d/meshenger/Utils.java
@@ -276,7 +276,13 @@ class Utils {
                     if (addr.isLoopbackAddress()) {
                         continue;
                     }
-                    addressList.add(new AddressEntry(addr.getHostAddress(), nif.getName(), addr.isMulticastAddress()));
+                    String fullAddress = addr.getHostAddress();
+                    if(fullAddress.indexOf('%')>0 && !fullAddress.contains("wlan")){
+                        String subaddress = fullAddress.substring(0, fullAddress.indexOf('%'));
+                        addressList.add(new AddressEntry(subaddress, nif.getName(), addr.isMulticastAddress()));
+                    } else {
+                        addressList.add(new AddressEntry(fullAddress, nif.getName(), addr.isMulticastAddress()));
+                    }
                 }
             }
         } catch (Exception ex) {
